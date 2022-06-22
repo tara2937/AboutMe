3ab83b1fded7c0b64c43e696718fa49538579694
Tara Ayer <hitaraayer@gmail.com>
2022-06-22 15:55:43 -0500

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Scholar on 6/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
