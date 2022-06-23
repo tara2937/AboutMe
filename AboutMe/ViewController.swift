5ba8d39e30ec203d903767e9325f8efcb06d15db
Tara Ayer <hitaraayer@gmail.com>
2022-06-23 11:46:44 -0500

    iteration one

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -13,7 +13,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+   
+    
+  
+    var favoriteImages : [UIImage] =  [UIImage(named :"lalaland")!, UIImage(named :"song")!, UIImage(named :"thekiterunner")!]
+    var favoriteLabels : [String] = ["Movie", "Album", "Book"]
+    
+    @IBOutlet weak var favImage: UIImageView!
+    @IBOutlet weak var favoriteLabel: UILabel!
+    
+    @IBAction func favoritesButton(_ sender: Any) {
+        var randomNumber = Int.random(in :0...2)
+        favImage.image = favoriteImages[randomNumber]
+        favoriteLabel.text = favoriteLabels[randomNumber]
+    }
+    
 }
 
